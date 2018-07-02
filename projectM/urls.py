50e0d289cca7c3cba06333d8790a636f3dd8b8c6
Vege <visakhatek@gmail.com>
2018-07-01 21:34:55 -0400

    Added Signup

--- projectM/urls.py
+++ projectM/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^home/$',views.home,name = 'home'),
     url(r'^login/$',views.login,name = 'login'),
+    url(r'^signup/$',views.signup,name = 'signup'),
 ]
 
 handler404 = 'projectM.views.error_404'
--- projectM/views.py
+++ projectM/views.py
@@ -6,5 +6,8 @@ def home(request):
 def login(request):
     return render(request,'login.html')
 
+def signup(request):
+    return render(request,'signup.html')
+
 def error_404(request,exception):
     return render(request,'error.html',{'data1':'page not found'})
